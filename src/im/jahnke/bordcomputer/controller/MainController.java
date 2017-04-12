2c93de9e7f5d670015e40164651b27716066177a
Dominik <Dominik@Dominik-Laptop>
2017-04-13 00:00:13 +0200

    fixed merge

--- src/im/jahnke/bordcomputer/controller/MainController.java
+++ src/im/jahnke/bordcomputer/controller/MainController.java
@@ -1,7 +1,6 @@
 package im.jahnke.bordcomputer.controller;
 
 import java.awt.event.ActionEvent;
-import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.File;
